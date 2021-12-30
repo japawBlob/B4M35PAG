30aba1a2a82daf8ead173f7a53a69880e7fe733b
Jakub Jíra <jakub.jira@rixo.cz>
2021-12-30 21:56:09 +0100

    working very naive version

---
+++ hw_3_code_skeletons/cmake/cmake-build-debug/CMakeFiles/HeatDiffusion.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HeatDiffusion.dir/mnt/c/Users/JakubJíra/Codes/B4M35PAG/hw_3_code_skeletons/src/HeatDiffusion.cpp.o"
+  "HeatDiffusion"
+  "HeatDiffusion.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HeatDiffusion.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- hw_3_code_skeletons/src/HeatDiffusion.cpp
+++ hw_3_code_skeletons/src/HeatDiffusion.cpp
@@ -17,6 +17,8 @@ struct Spot {
     int mY;
     float mTemperature;
 
+
+
     bool operator==(const Spot &b) const {
         return (mX == b.mX) && (mY == b.mY);
     }
@@ -76,59 +78,289 @@ void printHelpPage(char *program) {
 
 int main(int argc, char **argv) {
     // Initialize MPI
-    MPI_Init(&argc, &argv);
-    int worldSize, myRank;
-    MPI_Comm_size(MPI_COMM_WORLD, &worldSize);
-    MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
+    //MPI_Init(&argc, &argv);
+    //int worldSize, myRank;
+    //MPI_Comm_size(MPI_COMM_WORLD, &worldSize);
+    //MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
 
-    cout << "My rank: " << myRank << endl;
+    //cout << "My rank: " << myRank << endl;
 
     if (argc == 1) {
-        if (myRank == 0) {
+        //if (myRank == 0) {
             printHelpPage(argv[0]);
-        }
-        MPI_Finalize();
+        //}
+        //MPI_Finalize();
         exit(0);
     } else if (argc != 3) {
-        if (myRank == 0) {
+        //if (myRank == 0) {
             printHelpPage(argv[0]);
-        }
-        MPI_Finalize();
+        //}
+        //MPI_Finalize();
         exit(1);
     }
 
     // Read the input instance.
     int width, height;  // Width and height of the matrix.
     vector<Spot> spots; // Spots with permanent temperature.
-    if (myRank == 0) {
+    //if (myRank == 0) {
         tie(width, height, spots) = readInstance(argv[1]);
-    }
+    //}
+    //if(myRank == 0) {
+        high_resolution_clock::time_point start = high_resolution_clock::now();
 
-    high_resolution_clock::time_point start = high_resolution_clock::now();
 
-
-    //-----------------------\\
+        //-----------------------\\
     // Insert your code here \\
     //        |  |  |        \\
     //        V  V  V        \\
-
     // TODO: Fill this array on processor with rank 0. It must have height * width elements and it contains the
-    // linearized matrix of temperatures in row-major order
-    // (see https://en.wikipedia.org/wiki/Row-_and_column-major_order)
-    vector<float> temperatures;
+        // linearized matrix of temperatures in row-major order
+        // (see https://en.wikipedia.org/wiki/Row-_and_column-major_order)
+        vector<vector<float>> temp(height, vector<float>(width, 128));
+        vector<vector<float>> blobMatrix(height, vector<float>(width, 128));
+        vector<vector<bool>> permaSpots(height, vector<bool>(width, false));
 
-    //-----------------------\\
+        for (auto blob: spots) {
+            temp[blob.mY][blob.mX] = blob.mTemperature;
+            permaSpots[blob.mY][blob.mX] = true;
+            std::cout << "x: " << blob.mX << "  | y: " << blob.mY << "  | temperature: " << blob.mTemperature
+                      << std::endl;
+        }
+
+        bool continueComputation = false;
+        const float limite = 0.00001f;
+
+        /*for (int i = 0; i < height; ++i) {
+            for (int j = 0; j < width; ++j) {
+                std::cout << temp[i][j] << " ";
+                //temperatures.push_back(temp[i][j]);
+            }
+            std::cout << std::endl;
+        }*/
+
+        do {
+            bool checkForContinue = true;
+            continueComputation = false;
+
+            int i = 0, j = 0;
+
+            //roh v levo nahore
+            blobMatrix[i][j] = temp[i][j];
+            temp[i][j] = (temp[i][j] + temp[i][j + 1] + temp[i + 1][j] + temp[i + 1][j + 1]) / 4;
+            if (/*checkForContinue && */abs(blobMatrix[i][j] - temp[i][j]) > limite) {
+                bool skip = false;
+                for (auto blob: spots) {
+                    if (i == blob.mY && j == blob.mX) {
+                        temp[i][j] = blob.mTemperature;
+                        skip = true;
+                    }
+                }
+                if (!skip) {
+                    //std::cout << "1)      x: " << j << "  | y: " << i << "  | delta: " << abs(blobMatrix[i][j] - temp[i][j]) << std::endl;
+                    continueComputation = true;
+                    checkForContinue = false;
+                }
+            }
+
+            //prvni radek
+            for (j = 1; j < width - 1; ++j) {
+                blobMatrix[i][j] = temp[i][j];
+                temp[i][j] = (blobMatrix[i][j - 1] + temp[i][j] + temp[i][j + 1] + temp[i + 1][j - 1] + temp[i + 1][j] +
+                              temp[i + 1][j + 1]) / 6;
+                if (/*checkForContinue &&*/ (abs(blobMatrix[i][j] - temp[i][j]) > limite)) {
+                    bool skip = false;
+                    for (auto blob: spots) {
+                        if (i == blob.mY && j == blob.mX) {
+                            temp[i][j] = blob.mTemperature;
+                            skip = true;
+                        }
+                    }
+                    if (!skip) {
+                        //std::cout << "2)      x: " << j << "  | y: " << i << "  | delta: " << abs(blobMatrix[i][j] - temp[i][j]) << std::endl;
+                        continueComputation = true;
+                        checkForContinue = false;
+                    }
+                }
+            }
+
+            //roh v pravo nahore
+            blobMatrix[i][j] = temp[i][j];
+            temp[i][j] = (blobMatrix[i][j - 1] + temp[i][j] + temp[i+1][j] + temp[i + 1][j - 1]) / 4;
+            if (/*checkForContinue && */(abs(blobMatrix[i][j] - temp[i][j]) > limite)) {
+                bool skip = false;
+                for (auto blob: spots) {
+                    if (i == blob.mY && j == blob.mX) {
+                        temp[i][j] = blob.mTemperature;
+                        skip = true;
+                    }
+                }
+                if (!skip) {
+                    //std::cout << "3)      x: " << j << "  | y: " << i << "  | delta: " << abs(blobMatrix[i][j] - temp[i][j]) << std::endl;
+                    continueComputation = true;
+                    checkForContinue = false;
+                }
+            }
+            //hlavni cast matice
+            for (i = 1; i < height - 1; ++i) {
+                j = 0;
+                //levy kraj
+                blobMatrix[i][j] = temp[i][j];
+                temp[i][j] =
+                        (blobMatrix[i - 1][j] + blobMatrix[i - 1][j + 1] + temp[i][j] + temp[i][j + 1] +
+                         temp[i + 1][j] +
+                         temp[i + 1][j + 1]) / 6;
+                if (/*checkForContinue && */(abs(blobMatrix[i][j] - temp[i][j]) > limite)) {
+                    bool skip = false;
+                    for (auto blob: spots) {
+                        if (i == blob.mY && j == blob.mX) {
+                            temp[i][j] = blob.mTemperature;
+                            skip = true;
+                        }
+                    }
+                    if (!skip) {
+                        //std::cout << "4)      x: " << j << "  | y: " << i << "  | delta: " << abs(blobMatrix[i][j] - temp[i][j]) << std::endl;
+                        continueComputation = true;
+                        checkForContinue = false;
+                    }
+                }
+                //radek
+                for (j = 1; j < width - 1; ++j) {
+                    blobMatrix[i][j] = temp[i][j];
+                    temp[i][j] = (blobMatrix[i - 1][j - 1] + blobMatrix[i - 1][j] + blobMatrix[i - 1][j + 1] +
+                                  blobMatrix[i][j - 1] + temp[i][j] + temp[i][j + 1] + temp[i + 1][j - 1] +
+                                  temp[i + 1][j] +
+                                  temp[i + 1][j + 1]) / 9;
+                    //temperatures.push_back(temp[i][j]);
+
+                    if (/*checkForContinue && */(abs(blobMatrix[i][j] - temp[i][j]) > limite)) {
+                        bool skip = false;
+                        for (auto blob: spots) {
+                            //std::cout << "5) j: " << j << "  | i: " << i << "  | x: " << blob.mX << "  | y: " << blob.mY << std::endl;
+                            if (i == blob.mY && j == blob.mX) {
+                                temp[i][j] = blob.mTemperature;
+                                skip = true;
+                                break;
+                            }
+                        }
+                        if (!skip) {
+                            //std::cout << "5)      x: " << j << "  | y: " << i << "  | delta: " << abs(blobMatrix[i][j] - temp[i][j]) << "  | temp: " << blobMatrix[i][j] << std::endl;
+                            continueComputation = true;
+                            checkForContinue = false;
+                        }
+                    }
+                }
+                // pravy kraj radku
+                blobMatrix[i][j] = temp[i][j];
+                temp[i][j] = (blobMatrix[i - 1][j - 1] + blobMatrix[i - 1][j] + blobMatrix[i][j - 1] + temp[i][j] +
+                              temp[i + 1][j - 1] + temp[i + 1][j]) / 6;
+                if (/*checkForContinue && */(abs(blobMatrix[i][j] - temp[i][j]) > limite)) {
+                    bool skip = false;
+                    for (auto blob: spots) {
+                        if (i == blob.mY && j == blob.mX) {
+                            temp[i][j] = blob.mTemperature;
+                            skip = true;
+                        }
+                    }
+                    if (!skip) {
+                        //std::cout << "6)      x: " << j << "  | y: " << i << "  | delta: " << abs(blobMatrix[i][j] - temp[i][j]) << std::endl;
+                        continueComputation = true;
+                        checkForContinue = false;
+                    }
+                }
+            }
+            //levy dolni roh
+            j = 0;
+            blobMatrix[i][j] = temp[i][j];
+            temp[i][j] = (blobMatrix[i - 1][j] + blobMatrix[i - 1][j + 1] + temp[i][j] + temp[i][j + 1]) / 4;
+            if (/*checkForContinue && */(abs(blobMatrix[i][j] - temp[i][j]) > limite)) {
+                bool skip = false;
+                for (auto blob: spots) {
+                    if (i == blob.mY && j == blob.mX) {
+                        temp[i][j] = blob.mTemperature;
+                        skip = true;
+                    }
+                }
+                if (!skip) {
+                    //std::cout << "7)      x: " << j << "  | y: " << i << "  | delta: " << abs(blobMatrix[i][j] - temp[i][j]) << std::endl;
+                    continueComputation = true;
+                    checkForContinue = false;
+                }
+            }
+            //posledni radek
+            for (j = 1; j < width - 1; ++j) {
+                blobMatrix[i][j] = temp[i][j];
+                temp[i][j] =
+                        (blobMatrix[i - 1][j - 1] + blobMatrix[i - 1][j] + blobMatrix[i - 1][j + 1] +
+                         blobMatrix[i][j - 1] +
+                         temp[i][j] + temp[i][j + 1]) / 6;
+                //temperatures.push_back(temp[i][j]);
+
+                if (/*checkForContinue && */(abs(blobMatrix[i][j] - temp[i][j]) > limite)) {
+                    bool skip = false;
+                    for (auto blob: spots) {
+                        if (i == blob.mY && j == blob.mX) {
+                            temp[i][j] = blob.mTemperature;
+                            skip = true;
+                        }
+                    }
+                    if (!skip) {
+                        //std::cout << "8)      x: " << j << "  | y: " << i << "  | delta: " << abs(blobMatrix[i][j] - temp[i][j]) << std::endl;
+                        continueComputation = true;
+                        checkForContinue = false;
+                    }
+                }
+            }
+            // pravy dolni roh
+            blobMatrix[i][j] = temp[i][j];
+            temp[i][j] = (blobMatrix[i - 1][j - 1] + blobMatrix[i - 1][j] + blobMatrix[i][j - 1] + temp[i][j]) / 4;
+            if (/*checkForContinue && */(abs(blobMatrix[i][j] - temp[i][j]) > limite)) {
+                bool skip = false;
+                for (auto blob: spots) {
+                    if (i == blob.mY && j == blob.mX) {
+                        temp[i][j] = blob.mTemperature;
+                        skip = true;
+                    }
+                }
+                if (!skip) {
+                    //std::cout << "9)      x: " << j << "  | y: " << i << "  | delta: " << abs(blobMatrix[i][j] - temp[i][j]) << std::endl;
+                    continueComputation = true;
+                    checkForContinue = false;
+                }
+            }
+
+            for (auto blob: spots) {
+                temp[blob.mY][blob.mX] = blob.mTemperature;
+                blobMatrix[blob.mY][blob.mX] = blob.mTemperature;
+            }
+        } while (continueComputation);
 
 
-    double totalDuration = duration_cast<duration<double>>(high_resolution_clock::now() - start).count();
-    cout << "computational time: " << totalDuration << " s" << endl;
+        vector<float> temperatures(width * height, 0);
 
-    if (myRank == 0) {
-        string outputFileName(argv[2]);
-        writeOutput(myRank, width, height, outputFileName, temperatures);
-    }
+        for (int i = 0; i < height; ++i) {
+            for (int j = 0; j < width; ++j) {
+                temperatures[(height) * i + j] = temp[i][j];
+                //temperatures.push_back(temp[i][j]);
+            }
+        }
 
-    MPI_Finalize();
+
+
+
+
+        //-----------------------\\
+
+
+        double totalDuration = duration_cast<duration<double>>(high_resolution_clock::now() - start).count();
+        cout << "computational time: " << totalDuration << " s" << endl;
+
+        //if (myRank == 0) {
+            string outputFileName(argv[2]);
+            writeOutput(0, width, height, outputFileName, temperatures);
+        //}
+    //}
+    //MPI_Finalize();
+    exit(0);
     return 0;
 }
 
